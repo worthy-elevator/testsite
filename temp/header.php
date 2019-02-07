965c35a759fd9ff2c5914180ccf10ffc91b4993c
worthy-elevator <emil_96@inbox.ru>
2019-02-07 15:32:58 +0300

    Update header.php

--- temp/header.php
+++ temp/header.php
@@ -40,6 +40,7 @@ function menu2(){
   <meta http-equiv="content-type" content="text/html; charset=utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
   <link href="css/style.css" type="text/css" rel="stylesheet">
+  <link href="css/style-mobile.css" type="text/css" rel="stylesheet">
 </head>
 <body>
 	<div class="header">
